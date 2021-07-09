b322bd46543c035a5b925b464fb4d23c7cf2b114
s-a-n-d-y <mr.sandipan.das@gmail.com>
2021-07-09 12:40:06 +0200

    Seperated out model for code clarity

--- training/unet_training.py
+++ training/unet_training.py
@@ -2,6 +2,7 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from data.Dataloader import *
+from models.unet_model import *
 
 # Normal pythonic imports
 import numpy as np
@@ -13,19 +14,6 @@ from PIL import Image
 
 # TF libraries
 import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras.layers import *
-from tensorflow.keras.models import *
-import tensorflow.keras.backend as K
-import tensorflow as tf
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import Convolution2D, ZeroPadding2D, MaxPooling2D, Cropping2D, Conv2D
-from tensorflow.keras.layers import Input, Add, Dropout, Permute, add
-from tensorflow.compat.v1.layers import conv2d_transpose
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
-
 
 def dataAugmentGenerator(img_dir, frames_datagen, masks_datagen, frames_folder, masks_folder, seed = 1, batch_size = 5):
     '''Train Image data generator
@@ -52,124 +40,15 @@ def dataAugmentGenerator(img_dir, frames_datagen, masks_datagen, frames_folder, 
         yield X1i[0], np.asarray(mask_encoded)
 
 
-def unet(n_filters = 16, bn = True, dilation_rate = 1):
-    '''Validation Image data generator
-        Inputs: 
-            n_filters - base convolution filters
-            bn - flag to set batch normalization
-            dilation_rate - convolution dilation rate
-        Output: Unet keras Model
-    '''
-    #Define input batch shape
-    batch_shape=(256,256,3)
-    inputs = Input(batch_shape=(5, 256, 256, 3))
-    print(inputs)
-    
-    conv1 = Conv2D(n_filters * 1, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(inputs)
-    if bn:
-        conv1 = BatchNormalization()(conv1)
-        
-    conv1 = Conv2D(n_filters * 1, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv1)
-    if bn:
-        conv1 = BatchNormalization()(conv1)
-    
-    pool1 = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(conv1)
-
-    conv2 = Conv2D(n_filters * 2, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(pool1)
-    if bn:
-        conv2 = BatchNormalization()(conv2)
-        
-    conv2 = Conv2D(n_filters * 2, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv2)
-    if bn:
-        conv2 = BatchNormalization()(conv2)
-    
-    pool2 = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(conv2)
-
-    conv3 = Conv2D(n_filters * 4, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(pool2)
-    if bn:
-        conv3 = BatchNormalization()(conv3)
-        
-    conv3 = Conv2D(n_filters * 4, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv3)
-    if bn:
-        conv3 = BatchNormalization()(conv3)
-        
-    pool3 = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(conv3)
-
-    conv4 = Conv2D(n_filters * 8, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(pool3)
-    if bn:
-        conv4 = BatchNormalization()(conv4)
-        
-    conv4 = Conv2D(n_filters * 8, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv4)
-    if bn:
-        conv4 = BatchNormalization()(conv4)
-        
-    pool4 = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(conv4)
-
-    conv5 = Conv2D(n_filters * 16, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(pool4)
-    if bn:
-        conv5 = BatchNormalization()(conv5)
-        
-    conv5 = Conv2D(n_filters * 16, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv5)
-    if bn:
-        conv5 = BatchNormalization()(conv5)
-        
-    up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4], axis=3)
-    
-    conv6 = Conv2D(n_filters * 8, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(up6)
-    if bn:
-        conv6 = BatchNormalization()(conv6)
-        
-    conv6 = Conv2D(n_filters * 8, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv6)
-    if bn:
-        conv6 = BatchNormalization()(conv6)
-        
-    up7 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv3], axis=3)
-    
-    conv7 = Conv2D(n_filters * 4, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(up7)
-    if bn:
-        conv7 = BatchNormalization()(conv7)
-        
-    conv7 = Conv2D(n_filters * 4, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv7)
-    if bn:
-        conv7 = BatchNormalization()(conv7)
-        
-    up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv2], axis=3)
-    
-    conv8 = Conv2D(n_filters * 2, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(up8)
-    if bn:
-        conv8 = BatchNormalization()(conv8)
-        
-    conv8 = Conv2D(n_filters * 2, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv8)
-    if bn:
-        conv8 = BatchNormalization()(conv8)
-        
-    up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv1], axis=3)
-    
-    conv9 = Conv2D(n_filters * 1, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(up9)
-    if bn:
-        conv9 = BatchNormalization()(conv9)
-        
-    conv9 = Conv2D(n_filters * 1, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv9)
-    if bn:
-        conv9 = BatchNormalization()(conv9)
-        
-    conv10 = Conv2D(32, (1, 1), activation='softmax', padding = 'same', dilation_rate = dilation_rate)(conv9)
-
-    model = Model(inputs=inputs, outputs=conv10)
-
-    
-    return model
-
+##############################################################################################################
+############################# Sample Training code for plugging in later #####################################
+##############################################################################################################
 
 # Download data from here: http://mi.eng.cam.ac.uk/research/projects/VideoRec/CamSeq01/CamSeq01.zip
 img_dir = '/home/sandy/Projects/PhD/Courses/WASP/DL-GAN/WASP-DL-GAN-HA/HA3/Group/CamSeq01/'
 folder = ['train_frames/train', 'train_masks/train', 'val_frames/val', 'val_masks/val']
 camvid = Dataloader(img_dir, folder)
 camvid.visualize_sample(n_images_to_show=2)
-
-##############################################################################################################
-############################# Sample Training code for plugging in later #####################################
-##############################################################################################################
 # Normalizing only frame images, since masks contain label info
 data_gen_args = dict(rescale=1./255)
 mask_gen_args = dict()
@@ -183,6 +62,11 @@ train_frames_folder = folder[0].split('/')[0]
 train_masks_folder = folder[1].split('/')[0]
 val_frames_folder = folder[2].split('/')[0]
 val_masks_folder = folder[3].split('/')[0]
+
+##############################################################################################################
+############################### Here we can load our uncertainity models #####################################
+##############################################################################################################
+
 # Seed defined for aligning images and their masks
 seed = 1
 model = unet(n_filters = 32)
