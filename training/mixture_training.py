2a19f0941a9b5475e29f6cf874d6c1aeb022e08b
Oscar Carlsson <osccarls@chalmers.se>
2021-07-09 13:34:57 +0200

    Updated to TF2

--- training/mixture_training.py
+++ training/mixture_training.py
@@ -1,10 +1,82 @@
+import sys
+sys.path.append('../')
+
 from models import mixture_model
 from data import sample_generators
 
 import tensorflow as tf
+import tensorflow_probability as tfp
 
+import numpy as np
 
 def mixture_training(x_truth, y_truth, dropout, learning_rate, epochs, n_mixtures, display_step=2000):
+    """
+    Generic training of a Mixture Density Mixture Network for 2D data.
+
+    :param x_truth: training samples x
+    :param y_truth: training samples y / label
+    :param dropout:
+    :param learning_rate:
+    :param epochs:
+    :param n_mixtures: Number of mixtures in GMM
+    :param display_step:
+    :return: session, x_placeholder, dropout_placeholder
+    """
+    
+    if len(np.shape(x_truth))==1:
+        x_truth = np.expand_dims(x_truth, axis=-1)
+    
+    if len(np.shape(y_truth))==1:
+        y_truth = np.expand_dims(y_truth, axis=-1)
+    
+    build_shape = list(np.shape(x_truth))
+    build_shape[0]=None
+    
+    
+    model = mixture_model.MixtureModel(dropout, n_mixtures=n_mixtures)
+ 
+    model.build(tuple(build_shape))
+    
+    eps = 1e-4
+    
+    
+    def loss_fcn(ground_truth, gmm):
+        mixture_weights = gmm[0]
+        mixture_means = gmm[1]
+        mixture_variances = gmm[2]
+
+        dist = tfp.distributions.Normal(loc=mixture_means, scale=mixture_variances)
+        loss = - tf.reduce_mean(
+            tf.math.log(tf.reduce_sum(mixture_weights * dist.prob(ground_truth), axis=1) + eps),
+            axis=0
+        )
+        return loss
+    
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    
+    #@tf.function
+    def train_step(inputs, ground_truth):
+        with tf.GradientTape() as tape:
+            gmm, mean, uncertainties = model(inputs)
+            loss = loss_fcn(ground_truth, gmm)
+        grads = tape.gradient(loss, model.trainable_weights)
+        optimizer.apply_gradients(zip(grads, model.trainable_weights))
+        return loss, gmm, mean, uncertainties
+
+
+    for epoch in range(epochs):
+        loss, gmm, mean, uncertainties = train_step(x_truth, y_truth)
+
+        if epoch % display_step == 0:
+            print("Epoch {}".format(epoch))
+            print("Loss {}".format(loss))
+            print("================")
+
+    print("Training done!")
+    return model
+
+
+def mixture_training_old(x_truth, y_truth, dropout, learning_rate, epochs, n_mixtures, display_step=2000):
     """
     Generic training of a Mixture Density Mixture Network for 2D data.
 
@@ -66,7 +138,7 @@ def mixture_training(x_truth, y_truth, dropout, learning_rate, epochs, n_mixture
 
 if __name__ == "__main__":
     x, y = sample_generators.generate_osband_nonlinear_samples()
-    mixture_training(x, y, 0.3, 1e-3, 20000)
+    mixture_training(x, y, dropout=0.3, learning_rate=1e-3, epochs=1000, n_mixtures=4, display_step=100)
 
 
 
