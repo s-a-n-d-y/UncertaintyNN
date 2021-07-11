b0757c71f054ab6304705687d23c48d30542af1a
Oscar Carlsson <osccarls@chalmers.se>
2021-07-11 15:04:06 +0200

    Updated dropout mnist eval to TF2

--- evaluation/mnist_evaluation.py
+++ evaluation/mnist_evaluation.py
@@ -1,5 +1,8 @@
+import sys
+sys.path.append('../')
+
 from models import mnist_model
-from tensorflow.examples.tutorials.mnist import input_data
+# from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
 import numpy as np
@@ -13,7 +16,116 @@ import seaborn as sns
 from scipy.stats import bernoulli
 
 
-def mnist_dropout_evaluation(n_passes=50, dropout_rate=0.5, learning_rate=1e-4, epochs=10000, display_step=2000):
+def mnist_dropout_evaluation(n_passes=50, dropout_rate=0.5, learning_rate=1e-4, epochs=10, display_epoch=1):
+    """
+
+    :param n_passes:
+    :param dropout_rate:
+    :param learning_rate:
+    :param epochs:
+    :param display_step:
+    :return:
+    """
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    
+    x_data = tf.cast(x_train, dtype=tf.float32)
+    y_data = tf.cast(tf.keras.utils.to_categorical(y_train), dtype=tf.float32)
+    
+    x_test = tf.cast(x_test, dtype=tf.float32)
+    y_test = tf.cast(tf.keras.utils.to_categorical(y_test), dtype=tf.float32)
+    
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_data, y_data)).batch(50)  
+    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(100)  
+
+    model = mnist_model.DropoutCNNMnistModel(dropout_rate)
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate)
+    
+    num_batches = len(train_dataset)
+    
+    def train_step(input_data, ground_truth):
+        with tf.GradientTape() as tape:
+            logits, class_prob = model(input_data)
+            correct_prediction = tf.equal(tf.argmax(class_prob, 1), tf.argmax(ground_truth, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            loss = tf.compat.v1.losses.softmax_cross_entropy(onehot_labels=ground_truth, logits=logits)
+            
+        grads = tape.gradient(loss, model.trainable_weights)
+        optimizer.apply_gradients(zip(grads, model.trainable_weights))
+        return loss, accuracy
+
+    for epoch in range(epochs):
+        #batch = train_dataset[epoch%num_batches]
+
+        model.set_dropout_rate(dropout_rate)
+        accs = []
+        for step, (train_image_batch, train_label_batch) in enumerate(
+                    train_dataset
+                ):
+            _, acc = train_step(train_image_batch, train_label_batch)
+            accs.append(acc)
+            
+        train_accuracy = tf.reduce_mean(accs)
+        
+
+        if epoch % display_epoch == 0:
+            print("==================")
+            print("Epoch {}".format(epoch))
+            # cur_loss = sess.run(loss, feed_dict={x_data: batch[0],
+            #                                      y_data: batch[1]})
+            model.set_dropout_rate(0)
+            probs = []
+            labs = []
+            for _, (train_image_batch, train_label_batch) in enumerate(
+                    train_dataset
+                ):
+                _, class_prob = model(train_image_batch)
+                probs.append(class_prob)
+                labs.append(train_label_batch)
+            class_prob = tf.concat(probs, axis=0)
+            ground_truth = tf.concat(labs, axis=0)
+            correct_prediction = tf.equal(tf.argmax(class_prob, axis=1), tf.argmax(ground_truth, axis=1))
+            train_accuracy_zero_drop = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            print("Accuracy with dropout_rate 0: {}".format(train_accuracy_zero_drop))
+            print("Accuracy during training with droput_rate {}: {}".format(dropout_rate, train_accuracy))
+
+
+    # Running forwards passes
+    # MC Forward passes on test set
+    model.set_dropout_rate(0.5)
+    preds = []
+    labels = []
+    for _, (test_image_batch, test_label_batch) in enumerate(
+                test_dataset
+            ):
+        # Use tile instead of repeat, because np.repeat flattens results
+        x_batch_multipass = np.tile(test_image_batch, (n_passes,)+(len(np.shape(test_image_batch))-1)*(1,))
+
+        _, pred_y_multipass = model(x_batch_multipass)
+
+        pred_y_multipass = np.reshape(pred_y_multipass.numpy(),(-1, n_passes, 10), order="F")
+        pred_y_mean = tf.reduce_mean(pred_y_multipass, axis=1)
+        preds.append(pred_y_mean)
+        labels.append(test_label_batch)
+
+    pred_y_mean = tf.concat(preds, axis=0)
+    ground_truth = tf.concat(labels, axis=0)
+
+    correct_prediction = tf.equal(tf.argmax(pred_y_mean, 1), tf.argmax(ground_truth, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    print("Accurarcy using dropout_rate {}: {} ".format(0.5, accuracy))
+
+
+    # pred_y_multipass = pred_y_multipass.reshape(-1, n_passes)
+
+    # print('test accuracy %g' % accuracy.eval(feed_dict={
+    #     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+    # np.array([[e] * 10 for e in batch[0]]).reshape(-1, 500)
+
+
+def mnist_dropout_evaluation_old(n_passes=50, dropout_rate=0.5, learning_rate=1e-4, epochs=10000, display_step=2000):
     """
 
     :param n_passes:
@@ -138,5 +250,5 @@ def mnist_bootstrap_evaluation(n_heads=5, dropout_rate=0.3, learning_rate=1e-4, 
 
 
 if __name__ == "__main__":
-    # mnist_dropout_evaluation(epochs=10000)
-    mnist_bootstrap_evaluation(display_step=50)
+    mnist_dropout_evaluation(epochs=2)
+    # mnist_bootstrap_evaluation(display_step=50)
