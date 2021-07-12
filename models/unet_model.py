c7b2cb7f824b5cf988e1b2643be142f629e31657
Oscar Carlsson <osccarls@chalmers.se>
2021-07-12 11:08:31 +0200

    Cleared up spacing

--- models/unet_model.py
+++ models/unet_model.py
@@ -24,108 +24,73 @@ class Unet(tf.keras.Model):
         
         conv1 = Conv2D(n_filters * 1, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(inputs)
         conv1 = BatchNormalization()(conv1)
-
         conv1 = tf.keras.layers.Dropout(self.dropout_rate)(conv1)
-            
         conv1 = Conv2D(n_filters * 1, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv1)
         conv1 = BatchNormalization()(conv1)
-        
         conv1 = tf.keras.layers.Dropout(self.dropout_rate)(conv1)
-
         pool1 = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(conv1)
 
         conv2 = Conv2D(n_filters * 2, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(pool1)
         conv2 = BatchNormalization()(conv2)
-            
         conv2 = tf.keras.layers.Dropout(self.dropout_rate)(conv2)
-
         conv2 = Conv2D(n_filters * 2, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv2)
         conv2 = BatchNormalization()(conv2)
-        
         conv2 = tf.keras.layers.Dropout(self.dropout_rate)(conv2)
-
         pool2 = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(conv2)
 
         conv3 = Conv2D(n_filters * 4, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(pool2)
         conv3 = BatchNormalization()(conv3)
-            
         conv3 = tf.keras.layers.Dropout(self.dropout_rate)(conv3)
-
         conv3 = Conv2D(n_filters * 4, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv3)
         conv3 = BatchNormalization()(conv3)
-
         conv3 = tf.keras.layers.Dropout(self.dropout_rate)(conv3)
-            
         pool3 = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(conv3)
 
         conv4 = Conv2D(n_filters * 8, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(pool3)
         conv4 = BatchNormalization()(conv4)
-
         conv4 = tf.keras.layers.Dropout(self.dropout_rate)(conv4)
-            
         conv4 = Conv2D(n_filters * 8, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv4)
         conv4 = BatchNormalization()(conv4)
-
         conv4 = tf.keras.layers.Dropout(self.dropout_rate)(conv4)
-            
         pool4 = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(conv4)
 
         conv5 = Conv2D(n_filters * 16, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(pool4)
         conv5 = BatchNormalization()(conv5)
-
         conv5 = tf.keras.layers.Dropout(self.dropout_rate)(conv5)
-            
         conv5 = Conv2D(n_filters * 16, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv5)
         conv5 = BatchNormalization()(conv5)
-
         conv5 = tf.keras.layers.Dropout(self.dropout_rate)(conv5)
             
         up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4], axis=3)
-        
         conv6 = Conv2D(n_filters * 8, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(up6)
         conv6 = BatchNormalization()(conv6)
-
         conv6 = tf.keras.layers.Dropout(self.dropout_rate)(conv6)
-            
         conv6 = Conv2D(n_filters * 8, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv6)
         conv6 = BatchNormalization()(conv6)
-
         conv6 = tf.keras.layers.Dropout(self.dropout_rate)(conv6)
             
         up7 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv3], axis=3)
-        
         conv7 = Conv2D(n_filters * 4, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(up7)
         conv7 = BatchNormalization()(conv7)
-
         conv7 = tf.keras.layers.Dropout(self.dropout_rate)(conv7)
-            
         conv7 = Conv2D(n_filters * 4, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv7)
         conv7 = BatchNormalization()(conv7)
-
         conv7 = tf.keras.layers.Dropout(self.dropout_rate)(conv7)
             
         up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv2], axis=3)
-        
         conv8 = Conv2D(n_filters * 2, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(up8)
         conv8 = BatchNormalization()(conv8)
-
         conv8 = tf.keras.layers.Dropout(self.dropout_rate)(conv8)
-            
         conv8 = Conv2D(n_filters * 2, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv8)
         conv8 = BatchNormalization()(conv8)
-
         conv8 = tf.keras.layers.Dropout(self.dropout_rate)(conv8)
             
         up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv1], axis=3)
-        
         conv9 = Conv2D(n_filters * 1, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(up9)
         conv9 = BatchNormalization()(conv9)
-
         conv9 = tf.keras.layers.Dropout(self.dropout_rate)(conv9)
-            
         conv9 = Conv2D(n_filters * 1, (3, 3), activation='relu', padding = 'same', dilation_rate = dilation_rate)(conv9)
         conv9 = BatchNormalization()(conv9)
-
         conv9 = tf.keras.layers.Dropout(self.dropout_rate)(conv9)
             
         conv10 = Conv2D(self.output_classes + 1, (1, 1), padding = 'same', dilation_rate = dilation_rate)(conv9)
